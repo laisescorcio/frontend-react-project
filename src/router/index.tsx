a46c65951200cd9c4b59056882a9fb0a3d2b998d
Laís Escorcio <61030205+laisescorcio@users.noreply.github.com>
2025-04-21 18:58:20 -0300

    feat: navigate to login or dashboard depending on auth

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,11 +1,24 @@
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Routes, Route, BrowserRouter, Navigate } from "react-router-dom";
 import LoginPage from "../pages/LoginPage/LoginPage";
 import DashboardPage from "../pages/Dashboard/Dashboard";
 import { ProtectedRoute } from "./protectedRoute";
+import { useAuth } from "../providers/AuthProvider";
 export const RouterApp = () => {
+  const { isAuthenticated } = useAuth();
+
   return (
     <BrowserRouter>
       <Routes>
+        <Route
+          path="/"
+          element={
+            isAuthenticated ? (
+              <Navigate to="/dashboard" />
+            ) : (
+              <Navigate to="/login" />
+            )
+          }
+        />
         <Route
           path="/dashboard"
           element={
